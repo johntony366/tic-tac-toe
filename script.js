05d427b1b588ee5eacb5cadbc46781a88bc24653
John <johntony366@gmail.com>
2022-08-10 11:19:49 +0530

    Finish javascript code

--- script.js
+++ script.js
@@ -44,13 +44,41 @@ const displayController = (() => {
         });
     });
 
+    restartButton.addEventListener('click', () => {
+        gameBoard.reset();
+        gameController.reset();
+        resetMessage();
+
+        renderGameBoard();
+    })
+
     const renderGameBoard = () => {
         for (let index = 0; index < squares.length; index++) {
             squares[index].textContent = gameBoard.getSquare(index);
         }
     };
 
-    const setMessage = (msg) => {};
+    const setResultMessage = (sign) => {
+        setMessage(`Player ${sign} has won!`);
+    };
+
+    const setDrawMessage = () => {
+        setMessage('The game has ended in a draw!');
+    }
+
+    const setRoundMessage = (sign) => {
+        setMessage(`Player ${sign}'s turn`);
+    }
+    
+    const resetMessage = () => {
+        setMessage(`Player X's turn`);
+    };
+
+    const setMessage = (msg) => {
+        message.textContent = msg;
+    };
+
+    return { renderGameBoard, setResultMessage, setDrawMessage, setRoundMessage };
 })();
 
 const gameController = (() => {
@@ -62,13 +90,58 @@ const gameController = (() => {
 
     const playRound = (squareIndex) => {
         gameBoard.setSquare(squareIndex, getCurrentPlayerSign());
-        if (checkGameOver()) {
-            // Game is over
+        if (checkGameOver(squareIndex)) {
             gameOver = true;
-            // display work
             return;
         }
         ++round;
+        displayController.setRoundMessage(getCurrentPlayerSign());
+    };
+
+    const checkGameOver = (squareIndex) => {
+        if (round == 9) {
+            displayController.setDrawMessage();
+            return true;
+        } else if (checkWinner(squareIndex)) {
+            displayController.setResultMessage(
+                getCurrentPlayerSign()
+            );
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    const checkWinner = (squareIndex) => {
+        const winningCombinations = [
+            // rows
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+
+            // cols
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+
+            // diagonals
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        return winningCombinations
+            .filter((combination) => {
+                return combination.includes(squareIndex);
+            }) //This returns array of winning combinations which contain the index
+            .some((combination) => {
+                //Checking whether any are satisfied by current game board.
+                return combination.every((squareNumber) => {
+                    return (
+                        gameBoard.getSquare(squareNumber) ==
+                        getCurrentPlayerSign()
+                    );
+                });
+            });
     };
 
     const getGameOver = () => {
@@ -82,4 +155,11 @@ const gameController = (() => {
             return "O";
         }
     };
+
+    const reset = () => {
+        round = 1;
+        gameOver = false;
+    }
+
+    return { playRound, getGameOver, getCurrentPlayerSign, reset };
 })();
